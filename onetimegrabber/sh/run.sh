c1a4add16a42f3b4d53872f19905328060e978f8
Arun A K <arun.cetly@gmail.com>
2019-07-31 14:08:47 +0000

    run script

---
+++ onetimegrabber/sh/run.sh
@@ -0,0 +1,1 @@
+java -cp /home/hdfs/otg/lib/onetimegrabber-0.0.1.jar::/usr/hdp/current/hive-client/lib/*:/usr/hdp/current/hadoop-client/lib/*:/usr/hdp/current/hadoop-client/* com.ak.hive.ddlgrabber.onetimegrabber.HiveDDLOnetimeGrabber /home/hdfs/otg/conf/grabber.properties > /home/hdfs/otg/log/log.txt
