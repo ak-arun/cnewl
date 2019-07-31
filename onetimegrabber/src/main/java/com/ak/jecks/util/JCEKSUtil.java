51b8124f63b0830690f66ad4c6ec1b6d2e93df3b
Arun A K <arun.cetly@gmail.com>
2019-07-31 13:17:54 -0400

    JCEKS Util Class

---
+++ onetimegrabber/src/main/java/com/ak/jecks/util/JCEKSUtil.java
@@ -0,0 +1,28 @@
+package com.ak.jceks.util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+public class JCEKSUtil {
+	
+	public static KeyStore loadKeyStore(String fileName, char[] password)
+			throws KeyStoreException, IOException, NoSuchAlgorithmException,
+			CertificateException {
+		KeyStore ks = KeyStore.getInstance("JCEKS");
+		
+		FileInputStream fis = null;
+		try{
+			fis = new FileInputStream(fileName);
+		}catch(Exception e){
+			//ignore
+			}
+		ks.load(fis, password);
+		fis.close();
+		return ks;
+	}
+
+}
