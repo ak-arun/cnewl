81713f834b76f16b6c396638b11f6656a818b2c6
Arun A K <arun.cetly@gmail.com>
2019-08-08 19:17:45 +0000

    variable

--- onetimegrabber/sh/run.sh
+++ onetimegrabber/sh/run.sh
@@ -1,1 +1,2 @@
-nohup java -cp /home/hdfs/otg/lib/onetimegrabber-0.0.1.jar::/usr/hdp/current/hive-client/lib/*:/usr/hdp/current/hadoop-client/lib/*:/usr/hdp/current/hadoop-client/* com.ak.hive.ddlgrabber.onetimegrabber.HiveDDLOnetimeGrabber /home/hdfs/otg/conf/grabber.properties $pwd > /home/hdfs/otg/log/log.txt &
+jceksPwd="****"
+nohup java -cp /home/hdfs/otg/lib/onetimegrabber-0.0.1.jar::/usr/hdp/current/hive-client/lib/*:/usr/hdp/current/hadoop-client/lib/*:/usr/hdp/current/hadoop-client/* com.ak.hive.ddlgrabber.onetimegrabber.HiveDDLOnetimeGrabber /home/hdfs/otg/conf/grabber.properties $jceksPwd &
